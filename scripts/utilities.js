17de6ed97044e75b1d9b2176656a3c3a31a17ddb
Michelle Li <limichelle21@gmail.com>
2016-10-02 16:15:16 +0700

    assignment 23 forEach loop first attempt

---
+++ scripts/utilities.js
@@ -0,0 +1,10 @@
+function forEach(points, callback) {
+// loop through an array of items
+    for (var i = 0; i < points.length; i++) {
+        // callback(element)
+        callback(array[i]);
+    };
+}
+
+
+
